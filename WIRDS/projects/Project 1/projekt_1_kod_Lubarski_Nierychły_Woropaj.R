971d1d2d98745ef2bb5bd30a42a8f7846d7e7bd2
Jakub Woropaj <jakub.woropaj@gmail.com>
2015-03-25 22:51:50 +0100

    projekt 1 + kod
    istotne zmiany w kodzie i w wyglądzie

--- WIRDS/projects/Project 1/projekt_1_kod_Lubarski_Nierychły_Woropaj.R
+++ WIRDS/projects/Project 1/projekt_1_kod_Lubarski_Nierychły_Woropaj.R
@@ -3,23 +3,31 @@ library(dplyr)
 library(ggplot2)
 matury <- tbl_df(matury)
 
+matury$typ_szkoly <- factor(matury$typ_szkoly, levels = c("LO", "LOU", "LP", "T", "TU"),
+                            labels = c("Liceum Ogólnokształcące", "LO Uzupełniające", "Liceum Profilowane", "Technikum", "Technikum Uzupełniające"))
+
+library(ggthemes)
 # propozycja 3 ------------------------------------------------------------
 matury %>%
-  filter(wyniki_matur < 18) %>%
+  filter(ifelse(matura_nazwa == "geografia rozszerzona", wyniki_matur < 18, wyniki_matur < 15)) %>%
   count(typ_szkoly, rok) %>%
-  group_by(typ_szkoly) %>%
-  mutate(procent = n / sum(n)) %>%
+  group_by(rok) %>%
+  mutate(procent = n / sum(n),
+         csum = cumsum(procent) - procent/4) %>%
   ggplot(data = ., aes(x = rok,
                        y = procent,
                        fill = typ_szkoly,
                        group = typ_szkoly,
-                       label = round(procent, 2),
+                       label = round(procent, 3)*100,
                        ymax = 1)) +
   geom_bar(stat = "identity", position = "fill") +
-  ggtitle("Procentowy udział rok po roku \nniezaliczonych matur pod względem typu szkoły") +
+  ggtitle("Rokroczny procentowy udział niezaliczonych matur \nze względu na typ szkoły (w stosunku do ogółu)") +
   xlab("Rok pisania matury") +
-  ylab("Procent niezaliczonych") +
-  geom_text(position = "fill", vjust = 1.2, col = "white", size = 4) +
-  scale_fill_discrete(name = "Typ szkoły")
+  ylab("Procentowy udział niezaliczonych matur") +
+  geom_text(aes(y = csum), vjust = 1, col = "white", size = 4, stat = "identity") +
+  scale_fill_discrete(name = "Typ szkoły") +
+  scale_y_continuous(labels = c("0", "25", "50", "75", "100")) +
+  theme(plot.title = element_text(face = "bold")) +
+  theme_hc()
 
 
