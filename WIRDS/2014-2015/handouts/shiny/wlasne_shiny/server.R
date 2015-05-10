0757a9749c6624d4fc4eb524c408e4b47ed77dfb
Maciej Beręsewicz <MaciejBeresewicz@Berenz.local>
2015-05-10 19:12:36 +0200

    przykładowy panel

--- WIRDS/2014-2015/handouts/shiny/wlasne_shiny/server.R
+++ WIRDS/2014-2015/handouts/shiny/wlasne_shiny/server.R
@@ -3,4 +3,16 @@ library(ggplot2)
 library(knitr)
 
 ### data mungling
-
+shinyServer(
+  function(input, output) {
+    output$text <- renderText({
+      input$text
+    })
+    output$plot <- renderPlot({
+      x <- rnorm(input$n)
+      y <- rnorm(input$n)
+      plot(x,y)
+      title(main = input$title)
+    })
+  }
+)
--- WIRDS/2014-2015/handouts/shiny/wlasne_shiny/ui.R
+++ WIRDS/2014-2015/handouts/shiny/wlasne_shiny/ui.R
@@ -1,2 +1,28 @@
 ### first moves
 
+shinyUI(
+  fluidPage (
+    ### title of the panel
+    titlePanel('Hello world! I am alive!'),
+    ### sidebar
+    sidebarLayout(
+      ### sidebar
+      sidebarPanel(
+        textInput('title',
+                  'Wpisz tytuł wykresu:',
+                  value='Przykładowy tekst'),
+        numericInput(
+          'n',
+          'Wpisz liczbę powtórzeń',
+          value = 100
+        )
+      ),
+      ### main panel
+      mainPanel(
+        h3(textOutput('text')),
+        plotOutput('plot')
+      )
+    )
+  )
+
+)
