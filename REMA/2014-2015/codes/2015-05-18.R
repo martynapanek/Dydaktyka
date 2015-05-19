4461a0678830ed67dbb13df1cba588848520ca41
Maciej Beręsewicz <MaciejBeresewicz@Berenz-2.local>
2015-05-19 10:08:06 +0200

    GWR

--- REMA/2014-2015/codes/2015-05-18.R
+++ REMA/2014-2015/codes/2015-05-18.R
@@ -3,13 +3,24 @@ library(foreign)
 library(dplyr)
 library(tidyr)
 library(ggmap)
+library(ggplot2)
+library(AICcmodavg)
 
-baltimore <- read.dbf('REMA/2014-2015/datasets/baltim.dbf')
+baltimore <- read.dbf('~/Documents/Projects/RProjects/Dydaktyka/REMA/2014-2015/datasets/baltim.dbf')
 
 head(baltimore)
 
 simple_model <- lm(PRICE ~ NROOM + NBATH + GAR + AGE + SQFT, data = baltimore)
 summary(simple_model)
+
+
+plot(x = baltimore$PRICE,
+     y = simple_model$fitted.values,
+     main = 'Comparison of observed values (X) and prediction from LW (Y)',
+     xlab = 'Observed',
+     ylab = 'lm prediction')
+abline(a=0,b=1,col='red')
+
 
 ### setting the bandwidth (b)
 
@@ -32,9 +43,42 @@ model <- gwr(
     baltimore$Y),
   bandwidth = bandwidth)
 
+print(model)
+
+model <- gwr(
+  formula = PRICE ~ NROOM + NBATH + 
+    GAR + AGE + SQFT,
+  data = baltimore,
+  coords = cbind(
+    baltimore$X,
+    baltimore$Y),
+  bandwidth = bandwidth, 
+  hatmatrix = TRUE, ## hat matrix
+  se.fit = TRUE, ## standard errors
+  cl = 8, ## parallel computing
+  predictions = TRUE) ## save predictions
+
+print(model)
+
+## compare results
+
+plot(x = baltimore$PRICE,
+     y = model$SDF@data$pred,
+     main = 'Comparison of observed values (X) and prediction from GWR (Y)',
+     xlab = 'Observed',
+     ylab = 'GWR prediction')
+abline(a=0,b=1,col='red')
+
+plot(x = simple_model$fitted.values,
+     y = model$SDF@data$pred,
+     main = 'Comparison of prediction from LM (X) and GWR (Y)',
+     xlab = 'LM prediction',
+     ylab = 'GWR prediction')
+abline(a=0,b=1,col='red')
+
+
 ### visualisation
 
-library(ggplot2)
 
 str(model,1)
 str(model$SDF,2)
@@ -57,8 +101,8 @@ ggplot(
   geom_point(size=10)
 
 
-
 result %>%
+  select(`(Intercept)`:SQFT,X,Y) %>%
   gather(var,val,-X,-Y) %>%
   group_by(var) %>%
   mutate(val = scale(val)) %>%
@@ -67,20 +111,11 @@ result %>%
              y = Y,
              colour = val)) +
   geom_point(size = 5) +
-  facet_wrap(~var)
+  facet_wrap(~var) +
+  theme_bw()
 
-### how much we gained?
-var(result$pred)/var(baltimore$PRICE)
+### how much did we gain?
+cat('LM AICc:', AICc(simple_model),
+    'GWR AICc:', model$results$AICc)
 
-
-### plot results on map (however data is rescaled not to be indentified)
-hdf <- get_map('Baltimore,Maryland',zoom = 12)
-
-map <- ggmap('')
-
-ggmap(hdf) + 
-  geom_point(data = result,
-             aes(x = X,
-                 y = Y,
-                 colour = NBATH), size=10)
 
